7fbf330edfc9a4d228cbfef986f2439e32278ee3
tiagosousaoliveira <tiagosousaoliv@gmail.com>
2015-09-01 17:55:38 -0300

    ERP Versao 1.0 Eventos da VHome
    addActionListener(new ActionListener() {
    @Override
    public void actionPerformed(ActionEvent e) {
    // TODO Auto-generated method stub
    
    }
    });

--- source/br/com/DnSystem/View/VHome.java
+++ source/br/com/DnSystem/View/VHome.java
@@ -3,11 +3,14 @@ package br.com.DnSystem.View;
 import java.awt.Dimension;
 import java.awt.ScrollPane;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
 import javax.swing.JToolBar;
 
 public class VHome extends JFrame{
@@ -17,7 +20,7 @@ public class VHome extends JFrame{
 	JButton  Financeiro   = new JButton ();
 	JLabel	 lcadastros;  
 	JLabel	 lfinanceiro;
-	
+	JMenu
 	public VHome() {
 		// TODO Auto-generated constructor stub
 		super("Tela Inicial");
@@ -25,18 +28,36 @@ public class VHome extends JFrame{
 		
         Dimension ds = Toolkit.getDefaultToolkit().getScreenSize();  
         Dimension dw = getSize(); 
-        
         barra.setSize((ds.width - dw.width),50);
 
 		ImageIcon log = new ImageIcon(getClass().getResource("../img/livros.png"));
 		log.setImage(log.getImage().getScaledInstance(50, 35,35));
 		cadastros = new JButton(log);
 		cadastros.setToolTipText("Menu de Cadastros");
+		cadastros.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+				
+				
+			}
+		});
+		
+		
+		
 		
 		ImageIcon sen = new ImageIcon(getClass().getResource("../img/dinheiro.png"));
 		sen.setImage(sen.getImage().getScaledInstance(50, 35,35));
 		Financeiro = new JButton(sen);
 		Financeiro.setToolTipText("Modulo Financeiro");
+		Financeiro.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		
 
 		barra.add(cadastros);
